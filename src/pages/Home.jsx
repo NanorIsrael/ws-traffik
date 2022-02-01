33254c4f413cabca4922561e282e12c875bf311c
nanorissy <israel@alstergh.com>
2022-02-01 11:25:00 +0000

    pages added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import IntroSection from "../components/IntroSection";
+import InfoSection from "../components/InfoSection";
+
+function Home(props) {
+    return (
+        <>
+            <IntroSection />
+            <InfoSection />
+        </>
+    );
+}
+
+export default Home;
